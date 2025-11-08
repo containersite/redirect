bf99325f81ed84358a956b6ba5c4c008a8785e0c
containersite <sikdarparves527@gmail.com>
2025-11-09 01:00:08 +0600

    Delete site-one.js

--- site-one.js
+++
@@ -1,3 +0,0 @@
-        const watchURL = "https://videohubsa.blogspot.com/p/a.html";
-        const downloadURL = "https://videohubsa.blogspot.com/p/a.html";
-        const redirectURL = "https://zmistar.blogspot.com/";
