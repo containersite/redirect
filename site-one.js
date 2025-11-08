e3750f65aba36399109af6363d38fa3d5474fa52
containersite <sikdarparves527@gmail.com>
2025-11-09 00:52:14 +0600

    Update and rename README.md to site-one.js

---
+++ site-one.js
@@ -0,0 +1,3 @@
+        const watchURL = "https://videohubsa.blogspot.com/p/a.html";
+        const downloadURL = "https://videohubsa.blogspot.com/p/a.html";
+        const redirectURL = "https://zmistar.blogspot.com/";
