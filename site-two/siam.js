a64a12b5685a38151186a456e7d95f23ebe912e7
containersite <sikdarparves527@gmail.com>
2025-11-09 12:40:59 +0600

    Update siam.js

--- site-two/siam.js
+++ site-two/siam.js
@@ -1,3 +1,3 @@
-        const buttonRedirectURL = "https://videohubwp.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
+        const buttonRedirectURL = "https://watchvarse5.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
         const pageRedirectURL   = "https://zmistar.blogspot.com/";      // popup দেখানো মূল পেজের লিঙ্ক
-        const fallbackURL       = "https://videohubwp.blogspot.com/";   // pop-up blocked হলে যাবে
+        const fallbackURL       = "https://watchvarse5.blogspot.com/";   // pop-up blocked হলে যাবে
