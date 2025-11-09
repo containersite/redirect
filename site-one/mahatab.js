d6d2b4acb4c6b0bddb53c1391be06fb13caff291
containersite <sikdarparves527@gmail.com>
2025-11-09 13:50:47 +0600

    Update mahatab.js

--- site-one/mahatab.js
+++ site-one/mahatab.js
@@ -1,3 +1,3 @@
         const watchURL = "https://watchvarse7.blogspot.com/p/blog-page.html";
         const downloadURL = "https://watchvarse7.blogspot.com/p/blog-page.html";
-        const redirectURL = "https://zmistar.blogspot.com/";
+        const redirectURL = "https://wekfdi.short.gy/UA6m1J";
