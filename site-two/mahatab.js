3b2b683dec60b0519316d231d5ffafbf9c992da0
containersite <sikdarparves527@gmail.com>
2025-11-09 13:51:10 +0600

    Update mahatab.js

--- site-two/mahatab.js
+++ site-two/mahatab.js
@@ -1,3 +1,3 @@
         const buttonRedirectURL = "https://watchvarse8.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
-        const pageRedirectURL   = "https://zmistar.blogspot.com/";      // popup দেখানো মূল পেজের লিঙ্ক
+        const pageRedirectURL   = "https://wekfdi.short.gy/UA6m1J";      // popup দেখানো মূল পেজের লিঙ্ক
         const fallbackURL       = "https://watchvarse8.blogspot.com/";   // pop-up blocked হলে যাবে
