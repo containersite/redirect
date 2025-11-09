5ac300b9314a006e56df134fb825e92cbb580b5d
containersite <sikdarparves527@gmail.com>
2025-11-09 16:25:11 +0600

    Update parves.js

--- site-one/parves.js
+++ site-one/parves.js
@@ -1,3 +1,3 @@
         const watchURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
         const downloadURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
-        const redirectURL = "https://zmistar.blogspot.com/";
+        const redirectURL = "https://wetds.short.gy/CLbDTS";
