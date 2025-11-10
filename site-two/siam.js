e7388838cf145691069f0502d588f9761b368fec
containersite <sikdarparves527@gmail.com>
2025-11-10 13:46:01 +0600

    Update siam.js

--- site-two/siam.js
+++ site-two/siam.js
@@ -1,3 +1,3 @@
-        const buttonRedirectURL = "https://watchvarse5.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
+        const buttonRedirectURL = "https://videotoday71.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
         const pageRedirectURL   = "https://ads4xa.blogspot.com/p/10-siam.html";      // popup দেখানো মূল পেজের লিঙ্ক
-        const fallbackURL       = "https://watchvarse5.blogspot.com/";   // pop-up blocked হলে যাবে
+        const fallbackURL       = "https://videotoday71.blogspot.com/";   // pop-up blocked হলে যাবে
