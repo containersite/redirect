e367eefb73a34ae458f9532f14b3407649283ab8
containersite <sikdarparves527@gmail.com>
2025-11-10 13:38:20 +0600

    Update siam.js

--- site-one/siam.js
+++ site-one/siam.js
@@ -1,3 +1,3 @@
         const watchURL = "https://watchvarse4.blogspot.com/p/blog-page.html";
         const downloadURL = "https://watchvarse4.blogspot.com/p/blog-page.html";
-        const redirectURL = "https://wetfs.short.gy/siam";
+        const redirectURL = "https://ads4xa.blogspot.com/p/10-siam.html";
