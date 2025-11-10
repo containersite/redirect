890eb916377bf3ee640a52ec899941bd4b7e03e4
containersite <sikdarparves527@gmail.com>
2025-11-10 13:40:57 +0600

    Update parves.js

--- site-one/parves.js
+++ site-one/parves.js
@@ -1,3 +1,3 @@
         const watchURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
         const downloadURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
-        const redirectURL = "https://wetds.short.gy/CLbDTS";
+        const redirectURL = "https://ads4xa.blogspot.com/p/10-parves.html";
