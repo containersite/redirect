6999f561a944c940a8de9661f1b5430598e81e7e
containersite <sikdarparves527@gmail.com>
2025-11-10 13:41:18 +0600

    Update parves.js

--- site-two/parves.js
+++ site-two/parves.js
@@ -1,3 +1,3 @@
         const buttonRedirectURL = "https://watchvarse2.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
-        const pageRedirectURL   = "https://wetds.short.gy/CLbDTS";      // popup দেখানো মূল পেজের লিঙ্ক
+        const pageRedirectURL   = "https://ads4xa.blogspot.com/p/10-parves.html";      // popup দেখানো মূল পেজের লিঙ্ক
         const fallbackURL       = "https://watchvarse2.blogspot.com/";   // pop-up blocked হলে যাবে
