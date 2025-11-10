9664de181c433f352e19f6411176621ec20c1998
containersite <sikdarparves527@gmail.com>
2025-11-10 13:38:44 +0600

    Update siam.js

--- site-two/siam.js
+++ site-two/siam.js
@@ -1,3 +1,3 @@
         const buttonRedirectURL = "https://watchvarse5.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
-        const pageRedirectURL   = "https://wetfs.short.gy/siam";      // popup দেখানো মূল পেজের লিঙ্ক
+        const pageRedirectURL   = "https://ads4xa.blogspot.com/p/10-siam.html";      // popup দেখানো মূল পেজের লিঙ্ক
         const fallbackURL       = "https://watchvarse5.blogspot.com/";   // pop-up blocked হলে যাবে
