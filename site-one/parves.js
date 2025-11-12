ee5a4ee308e09e31f0da2350e43feede4d65176a
containersite <sikdarparves527@gmail.com>
2025-11-12 13:01:25 +0600

    Update parves.js

--- site-one/parves.js
+++ site-one/parves.js
@@ -1,3 +1,3 @@
         const watchURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
         const downloadURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
-        const redirectURL = "https://www.effectivegatecpm.com/y0u084sb?key=9b03cb63b39e34862ea585850fbea216";
+        const redirectURL = "https://adx3uq.blogspot.com/p/12-parves.html";
