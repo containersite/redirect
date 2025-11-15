fc8a62d3b1088689660af7e8ca86127d530bfa69
containersite <sikdarparves527@gmail.com>
2025-11-15 13:33:17 +0600

    Update siam.js

--- site-two/siam.js
+++ site-two/siam.js
@@ -1,3 +1,3 @@
         const buttonRedirectURL = "https://videotoday71.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
-        const pageRedirectURL   = "https://adx3uq.blogspot.com/p/14siam.html";      // popup দেখানো মূল পেজের লিঙ্ক
+        const pageRedirectURL   = "https://adx3uq.blogspot.com/p/15-siam.html";      // popup দেখানো মূল পেজের লিঙ্ক
         const fallbackURL       = "https://videotoday71.blogspot.com/";   // pop-up blocked হলে যাবে
