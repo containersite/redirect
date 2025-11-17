4091d53049a1ba4a35aa74a28cc462bd369d1a23
containersite <sikdarparves527@gmail.com>
2025-11-17 17:14:21 +0600

    Update parves.js

--- site-two/parves.js
+++ site-two/parves.js
@@ -1,3 +1,3 @@
-        const buttonRedirectURL = "https://redtubea.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
+        const buttonRedirectURL = "https://watchvarse2.blogspot.com/";      // হ্যাঁ/না বাটনের লিঙ্ক
         const pageRedirectURL   = "https://www.effectivegatecpm.com/rze4wd8aa?key=74d74ecf1cacbe96436ceb718716741c";      // popup দেখানো মূল পেজের লিঙ্ক
-        const fallbackURL       = "https://redtubea.blogspot.com/";   // pop-up blocked হলে যাবে
+        const fallbackURL       = "https://watchvarse2.blogspot.com/";   // pop-up blocked হলে যাবে
