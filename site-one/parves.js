2368428f705ceecad1170370afab882aeda21144
containersite <sikdarparves527@gmail.com>
2025-11-18 02:21:46 +0600

    Update parves.js

--- site-one/parves.js
+++ site-one/parves.js
@@ -1,6 +1,6 @@
-// URL configuration
-const watchURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
-const downloadURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
-        const redirectURL = "https://adx3uq.blogspot.com/p/parves.html";
-
-        const joinURL = "https://valianttossczar.com/fz3ifw9n?key=255cb9b54aebe1dca0f89408b47e3fcf";
+        // Configuration
+        const config = {
+            watchURL: "https://bitly.cx/VjZJ",
+            downloadURL: "https://bitly.cx/VjZJ",
+            redirectURL: "https://bitly.cx/nW87"
+        };
