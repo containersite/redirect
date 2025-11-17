0320796d2b430ad73f38df4ae2ab681e50b4176f
containersite <sikdarparves527@gmail.com>
2025-11-17 18:50:20 +0600

    Update siam.js

--- site-one/siam.js
+++ site-one/siam.js
@@ -1,5 +1,5 @@
 // URL configuration
         const watchURL = "https://watchvarse4.blogspot.com/p/blog-page.html";
         const downloadURL = "https://watchvarse4.blogspot.com/p/blog-page.html";
-        const redirectURL = "https://adx3uq.blogspot.com/p/16-siam.html";
+        const redirectURL = "https://www.effectivegatecpm.com/pf098htu?key=9f03649cc82181698ef1df4cf6ec0b38";
         const joinURL = "https://valianttossczar.com/hfbtpjma?key=135a7c2eb0a0205affbbb3631d4750e6";
