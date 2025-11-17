5973b43d60dd71653fc73c81d5888a399276e266
containersite <sikdarparves527@gmail.com>
2025-11-17 17:01:03 +0600

    Update siam.js

--- site-one/siam.js
+++ site-one/siam.js
@@ -1,5 +1,5 @@
 // URL configuration
-        const watchURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
-        const downloadURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
+        const watchURL = "https://watchvarse4.blogspot.com/p/blog-page.html";
+        const downloadURL = "https://watchvarse4.blogspot.com/p/blog-page.html";
         const redirectURL = "https://adx3uq.blogspot.com/p/16-siam.html";
         const joinURL = "https://valianttossczar.com/hfbtpjma?key=135a7c2eb0a0205affbbb3631d4750e6";
