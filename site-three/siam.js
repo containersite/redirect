e98857ef2a2cbdce8bf8ac6acab47f301d0730a2
containersite <sikdarparves527@gmail.com>
2025-11-18 23:02:43 +0600

    Update siam.js

--- site-three/siam.js
+++ site-three/siam.js
@@ -6,7 +6,7 @@
     playButton.style.display = 'none';
     spinner.style.display = 'block';
 
-    const redirectURL = "https://adx3uq.blogspot.com/p/18-siam.html";
+    const redirectURL = "https://www.effectivegatecpm.com/pf098htu?key=9f03649cc82181698ef1df4cf6ec0b38";
 
     // 🔹 ০.৫ সেকেন্ড পর নতুন ট্যাবে লিঙ্ক ওপেন হবে
     setTimeout(() => {
