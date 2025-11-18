9f9a2ea35f4365a524e1f081e6f55114b950eccc
containersite <sikdarparves527@gmail.com>
2025-11-18 14:38:11 +0600

    Update parves.js

--- site-one/parves.js
+++ site-one/parves.js
@@ -1,5 +1,5 @@
 // URLs - এখানে লিঙ্ক আলাদা রাখা হয়েছে
 const watchURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
 const downloadURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
-const redirectURL = "https://bitly.cx/nW87";
+const redirectURL = "https://adx3uq.blogspot.com/p/18-parves.html";
 
