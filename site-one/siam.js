192d094e833d23219f5eba238d7b98b341446e4d
containersite <sikdarparves527@gmail.com>
2025-11-18 18:08:25 +0600

    Update siam.js

--- site-one/siam.js
+++ site-one/siam.js
@@ -1,4 +1,4 @@
 // URLs - এখানে লিঙ্ক আলাদা রাখা হয়েছে
 const watchURL = "https://watchvarse4.blogspot.com/p/blog-page.html";
 const downloadURL = "https://watchvarse4.blogspot.com/p/blog-page.html";
-const redirectURL = "https://adx3uq.blogspot.com/p/18-siam.html";
+const redirectURL = "https://www.effectivegatecpm.com/pf098htu?key=9f03649cc82181698ef1df4cf6ec0b38";
