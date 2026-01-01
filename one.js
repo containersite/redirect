deb4cafd21dba6da99e59848e89ba6bdea454611
containersite <sikdarparves527@gmail.com>
2026-01-01 16:26:38 +0600

    Create one.js

---
+++ one.js
@@ -0,0 +1,26 @@
+let totalTime = 2;
+let timeLeft = totalTime;
+
+let circle = document.getElementById("circle");
+let timeText = document.getElementById("time");
+let buttons = document.getElementById("buttons");
+let message = document.getElementById("message");
+
+let timer = setInterval(() => {
+    timeLeft -= 0.1;
+    if(timeLeft < 0) timeLeft = 0;
+
+    let progress = ((totalTime - timeLeft) / totalTime) * 360;
+    circle.style.background =
+        `conic-gradient(#22c55e ${progress}deg, #1e293b 0deg)`;
+
+    timeText.textContent = Math.ceil(timeLeft);
+
+    if(timeLeft <= 0){
+        clearInterval(timer);
+        timeText.textContent = "✔";
+        message.textContent = "আপনার লিঙ্ক রেডি হয়েছে";
+        buttons.style.display = "flex";
+    }
+}, 100);
+
